232b0359f14212826a7b7d2c4675c9af5e7887f2
Luan <luanmvcosta@gmail.com>
2025-08-15 08:15:04 -0300

    feat: create module login

---
+++ src/modules/login/screens/LoginScreen.tsx
@@ -0,0 +1,11 @@
+import { BackgroundImage } from "../styles/loginScreen.styles";
+
+const LoginScreen = () => {
+    return (
+    <div>
+        <BackgroundImage src="public\background.png"/>
+    </div>
+)
+}
+
+export default LoginScreen;
