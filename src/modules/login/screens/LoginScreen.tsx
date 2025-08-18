a1752e9a5e191937d442a6754f2d194748c65f11
Luan <luanmvcosta@gmail.com>
2025-08-18 10:59:26 -0300

    feat: insert state in login

--- src/modules/login/screens/LoginScreen.tsx
+++ src/modules/login/screens/LoginScreen.tsx
@@ -1,8 +1,23 @@
+import { useState } from "react";
 import Button from "../../../shared/buttons/button/button";
 import Input from "../../../shared/inputs/input/input";
 import { BackgroundImage, ContainerLogin, ContainerLoginScreen, LimitedContainer, LogoImage, TitleLogin } from "../styleds/loginScreen.style";
 
 const LoginScreen = () => {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleUsername = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setUsername(event.target.value);
+    }
+
+    const handlePassword = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setPassword(event.target.value);
+    }
+
+    const handleLogin = () => {
+        alert(`username: ${username}, password: ${password}`)
+    }
 
     return ( 
     <div>
@@ -12,9 +27,9 @@ const LoginScreen = () => {
             <LimitedContainer>
                 <LogoImage src="public\logo.png" />
                 <TitleLogin level={2}> LOGIN </TitleLogin>
-                <Input title="USUÁRIO" />
-                <Input title="SENHA" />
-                <Button type="primary" margin="64px 0px 16px 0px" >ENTRAR</Button>
+                <Input title="USUÁRIO" margin="32px 0px 0px" onChange={handleUsername} value={username} />
+                <Input type="password" title="SENHA" margin="32px 0px 0px" onChange={handlePassword} value={password} />
+                <Button type="primary" margin="64px 0px 16px 0px" onClick={handleLogin} >ENTRAR</Button>
             </LimitedContainer>
         </ContainerLogin>
         </ContainerLoginScreen>
--- src/shared/inputs/input/input.tsx
+++ src/shared/inputs/input/input.tsx
@@ -4,11 +4,12 @@ import { BoxInput, TitleInput } from "./input.styles";
 
 interface Props extends InputProps {
     title?: string;
+    margin?: string;
 }
 
-const Input = ({ title, ...props }: Props) => {
+const Input = ({ margin, title, ...props }: Props) => {
     return (
-        <BoxInput>
+        <BoxInput style={{ margin }}>
             {title && <TitleInput>{title}</TitleInput>}
             <InputAntd {...props} />
         </BoxInput> 
